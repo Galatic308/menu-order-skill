be4b3f990fe52debf818fafd61182cceebdee885
Galatic308 <74140450+Galatic308@users.noreply.github.com>
2022-07-20 18:06:25 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,10 +8,12 @@ class MenuOrder(MycroftSkill):
     @intent_file_handler('order.menu.intent')
     def handle_order_menu(self, message):
         self.speak_dialog('order.menu')
+        
+    @intent_file_handler('order.menu.intent')
     def order_food(self, message):
         self.message_intent('i would like a burger')
         self.speak_dialog('one burger coming right up')
-        
+    
     def order_food(self, message):
         self.message_intent('i would like a chicken sandwich')
         self.speak_dialog('one chicken sandwich coming right up')
@@ -19,41 +21,47 @@ class MenuOrder(MycroftSkill):
      def order_food(self, message):
          self.message_intent('i would like a pizza sandwich')
          self.speak_dialog('im sorry we dont have pizza on the menu')
-         
+        
+    @intent_file_handler('order.menu.intent')
     def order_side(self, message):
         self.speak_dialog('what would you like for a side')
         
      def order_side(self, message):
+        if message > order_side:
          self.message_intent('i would like some french fries')
          self.speak_dialog('french fires coming right')
-         
-    def order_side(self, message):
+        if message < order_side:
         self.message_intent('i would like some onion rings')
         self.speak_dialog('im sorry we dont have onion rings on the menu')
         
+    @intent_file_handler('order.menu.intent')
     def order_drink(self, message):
         self.speak_dialog('what would you like to drink')
         
+    @intent_file_handler('order.menu.intent')
     def order_drink(self, message):
+    
+        if message > order_drink:
         self.message_intent('i would like a coke')
         self.speak_dialog('one coke coming right up')
         
-    def order_drink(self, message):
+        if message > order_drink:
         self.message_intent('i would like a orange drink')
         self.speak_dialog('one orange drink coming right up')
         
-    def order_drink(self, message):
+        if message > order_drink:
         self.message_intent('i would like a sprite')
         self.speak_dialog('one sprite coming right up')
         
-    def order_drink(self, message):
+        if message < order_drink:
         self.message_intent('i would like a pepsi')
         self.speak_dialog('im sorry we only have coke products')
         
-    def order_drink(self, message):
+        if message < order_drink:
         self.message_intent('i would like a mountain dew')
         self.speak_dialog('im sorry we only have coke products')
         
+    @intent_file_handler('order.menu.intent')
     def confirm_your_order(self, message):
         self.message_intent('i would like to confirm my order')
         self.speak_dialog('would you like to confirm your order')
